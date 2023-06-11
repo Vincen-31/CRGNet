b9725e21ea55f2fe86c2d5b9c6597f66f02843b9
崔东飞 <60336738+Vincen-31@users.noreply.github.com>
2023-06-11 16:14:52 +0800

    Add files via upload

---
+++ labeltransZurich.py
@@ -0,0 +1,43 @@
+from tqdm import tqdm
+import numpy as np
+import cv2
+import os
+
+# 标签中每个RGB颜色的值
+VOC_COLORMAP = np.array([[0, 0, 0], [100, 100, 100], [0, 125, 0], [0, 255, 0], [150, 80, 0],
+                         [0, 0, 150], [255, 255, 0], [150, 150, 255]])
+# 标签其标注的类别
+VOC_CLASSES = ['Roads', 'Buildings', 'Trees', 'Grass', 'Bare Soil', 'Water', 'Rails', 'Pools']
+
+# 处理txt中的对应图像
+txt_path = r'E:\vencen\Project\Pycharm\CRGNet-main\dataset\zurich_test.txt'
+# 标签所在的文件夹
+label_file_path = r'E:\vencen\Project\Pycharm\CRGNet-main\Zurich\gt'
+# 处理后的标签保存的地址
+gray_save_path = r'E:\vencen\Project\Pycharm\CRGNet-main\Zurich\gt_new\\'
+
+# txt_path = r'E:\vencen\Project\Pycharm\CRGNet-main\dataset\zurich_train.txt'
+# # 标签所在的文件夹
+# label_file_path = r'E:\vencen\Project\Pycharm\CRGNet-main\Zurich\point\an1'
+# # 处理后的标签保存的地址
+# gray_save_path = r'E:\vencen\Project\Pycharm\CRGNet-main\Zurich\point_new\an1\\'
+
+with open(txt_path, 'r') as f:
+    file_names = f.readlines()
+    for name in tqdm(file_names):
+        name = name.strip('\n')  # 去掉换行符
+        label_name = name  # label文件名
+        if not os.path.exists(gray_save_path):
+            os.mkdir(gray_save_path)
+        label_url = os.path.join(label_file_path, label_name)
+
+        mask = cv2.imread(label_url)
+        mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)  # 通道转换
+        mask = mask.astype(int)
+        label_mask = np.zeros((mask.shape[0], mask.shape[1]), dtype=np.int16)
+        # 标签处理
+        for ii, label in enumerate(VOC_COLORMAP):
+            locations = np.all(mask == label, axis=-1)
+            label_mask[locations] = ii #+ 15
+        # 标签保存
+        cv2.imwrite(gray_save_path + label_name, label_mask)
